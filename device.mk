43743afddd24c45f602bf46c64322b0906a7264c
Hoang Pham Anh Duy <duy@etylix.me>
2023-10-23 13:24:36 +0700

    spes: Remove hardware/xiaomi namespace

--- device.mk
+++ device.mk
@@ -472,7 +472,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/qcom-caf/bootctrl \
-    hardware/xiaomi \
     hardware/google/interfaces \
     hardware/google/pixel
 
